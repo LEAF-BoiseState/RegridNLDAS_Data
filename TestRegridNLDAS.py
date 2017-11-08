9e0aa886f4f06e4e3e9416b7b969affbf4455677
LejoFlores <lejoflores@boisestate.edu>
2017-11-08 09:46:08 -0700

    Completed Python script and added NetCDF format to .gitignore

--- TestRegridNLDAS.py
+++ TestRegridNLDAS.py
@@ -1,16 +1,30 @@
 import pygrib as pg
 import numpy as np
 from scipy import interpolate
+from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 
-Elon = -110.0
-Wlon = -120.0
-Slat = 42.0
-Nlat = 48.0
+##
 
+# S
+NLDAS_res = 0.125
+
+# NLDAS file name
 nldas_file = 'NLDAS_FORA0125_H.A20100603.0800.002.grb'
-#wrf_lat_file = 
-#wrf_lon_file = 
+
+wrf_lat_file = 'xlat_d02.nc'
+wrf_lon_file = 'xlong_d02.nc'
+
+nc_wrf_lat_fid = Dataset(wrf_lat_file, 'r')
+nc_wrf_lon_fid = Dataset(wrf_lon_file, 'r')
+
+wrf_lats = nc_wrf_lat_fid.variables['XLAT'][0,:,:]
+wrf_lons = nc_wrf_lon_fid.variables['XLONG'][0,:,:]
+
+Sbnd = np.floor(wrf_lats.min()) + 2.0*NLDAS_res
+Nbnd = np.ceil(wrf_lats.max()) + 2.0*NLDAS_res
+Wbnd = np.floor(wrf_lons.min()) + 2.0*NLDAS_res
+Ebnd = np.ceil(wrf_lons.max()) + 2.0*NLDAS_res
 
 # Open the NLDAS grib file 
 grbs = pg.open(nldas_file) # Open the file
@@ -22,8 +36,8 @@ lat,lon = grb.latlons()
 lat1d = lat[:,0]
 lon1d = lon[0,:]
 
-id_lat = np.ix_(np.logical_and(lat1d>=Slat,lat1d<=Nlat))
-id_lon = np.ix_(np.logical_and(np.abs(lon1d)>=np.abs(Elon),np.abs(lon1d)<=np.abs(Wlon)))
+id_lat = np.ix_(np.logical_and(lat1d>=Sbnd,lat1d<=Nbnd))
+id_lon = np.ix_(np.logical_and(np.abs(lon1d)>=np.abs(Ebnd),np.abs(lon1d)<=np.abs(Wbnd)))
 
 id_lat_beg = np.min(id_lat)
 id_lat_end = np.max(id_lat)
@@ -45,9 +59,16 @@ points = np.stack((lon_ss_1d,lat_ss_1d),axis=-1)
 fNN = interpolate.NearestNDInterpolator(points, data_ss.flatten())
 
 # 3. Interpolate to lon and lat arrays from WRF
+NLDAS_wrfgrd = fNN(wrf_lons,wrf_lats)
 
+plt.figure(1)
+plt.subplot(211)
 plt.pcolor(lon_ss,lat_ss,data_ss)
 #plt.imshow(id_lat_lon)
+plt.colorbar()
+
+plt.subplot(212)
+plt.pcolor(wrf_lons,wrf_lats,NLDAS_wrfgrd)
 plt.colorbar()
 plt.show()
 
